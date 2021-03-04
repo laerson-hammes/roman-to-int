b72777b56ac3859deb9b2609d361d9cc40fdf6b1
laerson-hammes <laersonha@gmail.com>
2021-03-04 19:31:22 -0300

    Update roman-two-int.py

--- roman-two-int.py
+++ roman-two-int.py
@@ -1,17 +1,17 @@
 def roman2int(roman):
-   romansValues = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+   romans_values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
    conversion = 0
    for index, letter in enumerate(roman):
       if index + 1 < len(roman):
-         if romansValues[str.upper(roman[index + 1])] > romansValues[str.upper(letter)]:
-            conversion = conversion - romansValues[f"{str.upper(letter)}"]
+         if romans_values[str.upper(roman[index + 1])] > romans_values[str.upper(letter)]:
+            conversion = conversion - romans_values[f"{str.upper(letter)}"]
          else:
-            conversion = conversion + romansValues[f"{str.upper(letter)}"]
+            conversion = conversion + romans_values[f"{str.upper(letter)}"]
       else:
-         conversion = conversion + romansValues[f"{str.upper(letter)}"]
+         conversion = conversion + romans_values[f"{str.upper(letter)}"]
    return conversion
           
 
-roman = str(input("Informe o número romano desejado: "))
+roman = str(input("[+] ENTER THE DESIRED ROMAN NUMBER: "))
 conversion = roman2int(list(roman))
 print(f"{str.upper(roman)} = {conversion}")
